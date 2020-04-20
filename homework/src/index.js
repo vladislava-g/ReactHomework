0ca3e318aa6bff2fc4db4014e5578742007fc851
_glr <good.luck.3933@gmail.com>
2020-04-20 15:49:43 +0300

    Functional Components

---
+++ homework/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import MyInfoComponent from "./componets/MyInfoComponent";
+
+let myInfoComponent = <MyInfoComponent/>;
+
+ReactDOM.render(
+  myInfoComponent,
+  document.getElementById('root')
+);
