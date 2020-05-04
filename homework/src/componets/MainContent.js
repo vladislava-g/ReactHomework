ace30e2e1d3c59cd1c345687380a5cf6a63154a1
_glr <good.luck.3933@gmail.com>
2020-05-04 16:12:00 +0300

    Class Component State

--- homework/src/componets/MainContent.js
+++ homework/src/componets/MainContent.js
@@ -1,29 +1,14 @@
 import React from "react";
-import Joke from "./Joke";
-
-class JokeData {
-    constructor(punchLine, question) {
-        this.question = question;
-        this.punchLine = punchLine;
-    }
-}
+import TodoItem from "./TodoItem.js";
+import todosData from "./todosData";
 
 function MainContent() {
-    let jokes = [
-        new JokeData("They planet.", "How does NASA organize a party?"),
-        new JokeData("Because they're dead.", "Why can't dinosaurs clap? "),
-        new JokeData("It’s a good thing snakes and dogs don’t interbreed. Nobody wants a loyal snake."),
-        new JokeData("Cole’s Law: Thinly Sliced Cabbage"),
-        new JokeData("Between you and me, something smells.", "What did the eye say to the eye?")
-    ];
-
-    let colors = ["#dd6137", "#e3a960" ,"#52946b", "#4f859a", "#6cb6b1"];
 
     return (
         <main className="main">
             {
-                jokes.map((value, index) => {
-                    return <Joke data={value} color={colors[index]} />
+                todosData.map((value) => {
+                    return <TodoItem data={value} id={value.id}/>
                 })
             }
         </main>
--- homework/src/componets/Navbar.js
+++ homework/src/componets/Navbar.js
@@ -4,12 +4,13 @@ import "./Navbar.css";
 function Navbar() {
     return (
         <div id="navbar">
-            <ul>
+            {/* <ul>
                 <li><a href="#">Home</a></li>
                 <li><a href="#">News</a></li>
                 <li><a href="#">Contact</a></li>
                 <li><a href="#">About</a></li>
-            </ul>
+            </ul> */}
+            TO DO LIST
         </div>
     );
 }
